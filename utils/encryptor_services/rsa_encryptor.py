8ae78d387f511048b26f7044d3e49e1c795651d8
mohammadhnz0 <hmohammadali2013@gmail.com>
2023-06-29 12:21:05 +0330

    feat: initialize project

---
+++ utils/encryptor_services/rsa_encryptor.py
@@ -0,0 +1,47 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+class RSAEncryptor:
+    def __init__(self, public_key, private_key=None):
+        self.public_key = public_key
+        self.private_key = private_key
+
+    @staticmethod
+    def generate_key():
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        public_key = private_key.public_key()
+        public_pem = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        private_pem = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+        return public_pem, private_pem
+
+    def encrypt(self, plaintext):
+        ciphertext = self.public_key.encrypt(
+            plaintext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return ciphertext
+
+    def decrypt(self, ciphertext):
+        plaintext = self.private_key.decrypt(
+            ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return plaintext
